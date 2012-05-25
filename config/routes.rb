c3a53f3c7307cfc2d5ab1eecd71800f29827f423
Matt Young <matt@chicagowebguru.com>
2012-05-25 14:07:36 -0500

    Added mysql and about controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 MattsRails::Application.routes.draw do
+
+  root :to => "welcome#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'about/' => 'about#index'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
