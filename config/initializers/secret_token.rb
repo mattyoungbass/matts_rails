11986b98ca7d19b79708933b589811ffd7924d4d
Matt Young <matt@chicagowebguru.com>
2012-05-25 08:00:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MattsRails::Application.config.secret_token = 'e71d65ce21521bd75b4b915dfe275df54e40387c10d46dc65030d9ca8d44577b749344f8e82568f9d4b8fbeb96d4ea849fea9dcf4f6f232467d7f7daecd0faab'
